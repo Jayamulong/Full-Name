72b4472c2f9c9c30de13406d53e2fc8cf4b43901
jayandrey23 <jayandreyamulong@gmail.com>
2021-10-31 00:18:05 +0800

    Apples and Oranges Version 2

---
+++ Assignment 3/Part_2.py
@@ -0,0 +1,56 @@
+def Apple_Price():
+    Apple_Func = int(20)
+    return Apple_Func
+
+def Orange_price():
+    Orange_Func = int(25)
+    return Orange_Func
+
+def Display_Prices(ApplesF, OrangeF):
+    print(f"The price for an apple is {ApplesF}, and the price for an orange is {OrangeF}.")
+
+def Apple_Amount():
+    Apple_Number = int(input("How many apples you want to buy: "))
+    return Apple_Number
+
+def Orange_Amount():
+    Orange_Number = int(input("How many oranges you want to buy?: "))
+    return Orange_Number
+
+def Display_Amount(AppleA, OrangeA):
+    print(f"The amount of apples bought is {AppleA} and the amount of oranges bought is {OrangeA} ")
+
+def Calculate_Apple_Price():
+    Apples_Prices = 20*Apple_Sum
+    print(f"The total price of apples is {Apples_Prices}")
+    return Apples_Prices
+
+def Calculate_Orange_Price():
+    Oranges_Prices = 25*Orange_Sum
+    print(f"The total price of oranges is {Oranges_Prices}")
+    return Oranges_Prices
+
+def Total_Amount():
+    Total = Apples_Prices + Oranges_Prices
+    print(f"The total amount is {Total}")
+    
+
+# Steps:
+# # 1. Present the apple and the orange price
+Apples = Apple_Price()
+Oranges = Orange_price()
+Display_Prices(Apples, Oranges)
+# 2. Ask how many apples and orange will buy
+Apple_Sum = Apple_Amount()
+Orange_Sum = Orange_Amount()
+Display_Amount(Apple_Sum, Orange_Sum)
+#Calculate the amount and price of apples and oranges
+Apples_Prices = Calculate_Apple_Price()
+Oranges_Prices = Calculate_Orange_Price()
+#Give the total amount of apples and oranges combined
+Total_Amount()
+
+
+     
+
+   
